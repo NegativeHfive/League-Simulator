809912f5440bd8e2cb98a6f36753aa9b230f2f95
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-02-05 10:36:52 +0100

    busy witht the navbar

--- leaguehome.php
+++ leaguehome.php
@@ -18,7 +18,23 @@
 </head>
 <body>
 
-      
-    
+      <div class="homepage">
+
+        <div class="navbar">
+            <h2>negative.</h2>
+            <div class="icons">
+                <div class="home">
+                    <ion-icon name="home"></ion-icon>
+                    <a href="#">Home</a>
+                </div>
+            </div>
+        </div>
+
+      </div>
+
+
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
