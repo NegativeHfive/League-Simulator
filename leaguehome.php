8866de26bedbb721095a16cec7de3254a9db44f7
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-02-05 11:41:23 +0100

    home page 63% done

--- leaguehome.php
+++ leaguehome.php
@@ -14,24 +14,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>League Simulator</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./css/leaguehome.css">
 </head>
 <body>
 
-      <div class="homepage">
-
-        <div class="navbar">
+      <div class="navbar">
             <h2>negative.</h2>
             <div class="icons">
                 <div class="home">
                     <ion-icon name="home"></ion-icon>
                     <a href="#">Home</a>
                 </div>
+                <div class="about">
+                    <ion-icon name="man"></ion-icon>
+                    <a href="#">About Me</a>
+                </div>
+                <div class="help">
+                    <ion-icon name="help-circle"></ion-icon>
+                    <a href="#">Help</a>
+                </div>
             </div>
         </div>
 
-      </div>
+       <div class="menu">
+             <div class="title">
+                <h2>LEAGUE</h2>
+                <h1>Simulator</h1>
+                <a href="#" class="play">PLAY</a>
+            </div>
 
+            <div class="img">
+                <img class="mbappe" src="./images/French-soccer-player-Kylian-Mbappe-FIFA-World-Cup-December-10-2022-Photoroom.png" alt="">
+            </div>
+       </div>
+
+        
 
 
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
