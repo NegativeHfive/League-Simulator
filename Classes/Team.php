e43f3f3717c4cd46ec7f4c495441c710c61f7def
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-02-05 09:31:01 +0100

    home page beonnen mapen gemaakt

---
+++ Classes/Team.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once "../includes/Database.php";
+
+class Team{
+
+
+    private $db;
+
+    //constructor
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    //het tovoegen van nieuwe teams 
+    
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ leaguehome.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>League Simulator</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+
+      
+    
+</body>
+</html>
