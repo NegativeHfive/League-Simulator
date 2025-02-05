92bf3853d573fcf118bd0c694189f426007e0df6
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-02-05 09:01:02 +0100

    Database php klaar

--- includes/Database.php
+++ includes/Database.php
@@ -1,6 +1,43 @@
 <?php
 
+class Database{
 
+    public $pdo;
+    public $statement;
+
+    //constructor
+    public function __construct($db='league',$host='localhost:3310',$user='root',$pwd=''){
+        //try en catch voor meer duidelijkheid met fouten 
+        try{
+            $this->pdo = new PDO("mysql:host=$host;dbname=$db",$user,$pwd);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            echo "Connected to ".$db."succesfully";
+        }
+        catch(PDOException $e){
+            die('Connection error :'.$e->getMessage());
+        }
+    }
+
+    // het run functie 
+    public function run($query,$params = null){
+        try{
+            $this->statement = $this->pdo->prepare($query);
+            if($params != null){
+                $this->statement->execute($params);
+            }
+            else{
+                $this->statement->execute();
+            }
+            return $this->statement;
+        }
+        catch(PDOException $e){
+            echo "Execution error : ".$e->getMessage();
+            return false;
+        }
+    }
+
+
+}
 
 
 
